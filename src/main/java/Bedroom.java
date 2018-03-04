3656c356b2261215abd972124b164b7e9b7ed4d1
Jim Bevington <jim.bevington@hotmail.com>
2018-03-04 10:45:53 +0000

    added daysBooked parameter to Bedroom and ConferenceRoom

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,12 +1,14 @@
 public class Bedroom extends Room {
 
-    RoomType type;
-    double price;
+    private RoomType type;
+    private double price;
+    private int daysBooked;
 
     public Bedroom(int capacity, int number, RoomType type, double price) {
         super(capacity, number);
         this.type = type;
         this.price = price;
+        this.daysBooked = 0;
     }
 
     public RoomType getRoomType() {
--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -2,10 +2,12 @@ public class ConferenceRoom extends Room {
 
     private String name;
     private double price;
+    private int daysBooked;
 
     public ConferenceRoom(int capacity, int number, String name, double price) {
         super(capacity, number);
         this.name = name;
         this.price = price;
+        this.daysBooked = 0;
     }
 }
